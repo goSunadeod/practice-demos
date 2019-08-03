0506140747ea7c8531aa84d4446a089bd9a99da9
sundae <haiqiang.xu@hand-china.com>
2019-08-03 23:58:05 +0800

    升级react-app

--- react-hook/src/App.js
+++ react-hook/src/App.js
@@ -1,90 +1,26 @@
-import React, { useState, useRef, useEffect } from 'react';
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
 
-export default function Example() {
-    const [count, setCount] = useState(0);
-    const prevCount = usePrevious(count);
-    useEffect(() => {
-        document.title = `You clicked ${count} times`
-    })
-    return (
-      <div>
-          <p>You clicked {count} times</p>
-          <p>Now: {count}, before: {prevCount}</p>
-          <button onClick={() => setCount(count + 1)}>
-              Click me
-          </button>
-      </div>
-    )
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
-function usePrevious(value) {
-    const ref = useRef();
-    useEffect(() => {
-        ref.current = value;
-    });
-    return ref.current;
-}
-
-
-// export default function Example() {
-//     const [count, setCount] = useState(0);
-//
-//     function handleAlertClick() {
-//         setTimeout(() => {
-//             alert('You clicked on: ' + count);
-//         }, 3000);
-//     }
-//
-//     return (
-//       <div>
-//           <p>You clicked {count} times</p>
-//           <button onClick={() => setCount(count + 1)}>
-//               Click me
-//           </button>
-//           <button onClick={handleAlertClick}>
-//               Show alert
-//           </button>
-//       </div>
-//     );
-// }
-
-
-
-// const initialState = {count: 0};
-//
-// function reducer(state, action) {
-//     switch (action.type) {
-//         case 'increment':
-//             return {count: state.count + 1};
-//         case 'decrement':
-//             return {count: state.count - 1};
-//         default:
-//             throw new Error();
-//     }
-// }
-//
-// export default function Counter() {
-//     const [state, dispatch] = useReducer(reducer, initialState);
-//     return (
-//       <React.Fragment>
-//           Count: {state.count}
-//           <button onClick={() => dispatch({type: 'increment'})}>+</button>
-//           <button onClick={() => dispatch({type: 'decrement'})}>-</button>
-//       </React.Fragment>
-//     );
-// }
-
-//
-// export default function TextInputWithFocusButton() {
-//     const inputEl = useRef(null);
-//     const onButtonClick = () => {
-//         // `current` 指向已挂载到 DOM 上的文本输入元素
-//         inputEl.current.focus();
-//     };
-//     return (
-//       <React.Fragment>
-//           <input ref={inputEl} type="text" />
-//           <button onClick={onButtonClick}>Focus the input</button>
-//       </React.Fragment>
-//     );
-// }
+export default App;
--- react-hook/src/index.js
+++ react-hook/src/index.js
@@ -1,9 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
 import './index.css';
+import { HashRouter, Route, Switch } from 'react-router-dom';
+import App from './App';
+import UseState from './api/useState';
 
 ReactDOM.render(
-  <App />,
+  <div>
+    <ul>
+      <li><a href="#/">Home</a></li>
+      <li><a href="#/useState">UseState</a></li>
+      <li><a href="#/useEffect">UseEffect</a></li>
+      <li><a href="#/useContext">UseContext</a></li>
+      <li><a href="#/useReducer">UseReducer</a></li>
+      <li><a href="#/useCallback">UseCallback</a></li>
+      <li><a href="#/useMemo">UseMemo</a></li>
+      <li><a href="#/useRef">UseRef</a></li>
+      <li><a href="#/mobx">Mobx</a></li>
+      <li><a href="#/memoized">Memoized</a></li>
+    </ul>
+    <HashRouter>
+      <Switch>
+        <Route exact path="/" component={App}></Route>
+        <Route exact path="/useState" component={UseState}></Route>
+      </Switch>
+    </HashRouter>
+  </div>,
   document.getElementById('root')
 );
