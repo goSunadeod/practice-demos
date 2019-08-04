ed30ad773f776d7ccc8e9477d158b91319f19da2
sundae <haiqiang.xu@hand-china.com>
2019-08-04 19:28:57 +0800

    fix

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-demo 集合
+###demo 集合
+#### 学习分享：[前端学习链接](https://github.com/Nealyang/PersonalBlog/issues/48)
--- react-hook/src/index.js
+++ react-hook/src/index.js
@@ -4,6 +4,13 @@ import './index.css';
 import { HashRouter, Route, Switch } from 'react-router-dom';
 import App from './App';
 import UseState from './api/useState';
+import UseEffect from './api/useEffect';
+import UseRef from './api/useRef';
+import UseContext from './api/useContext';
+import UseCallback from './api/useCallback';
+import UseReducer from './api/useReducer';
+import UseMemo from './api/useMemo';
+import Memoized from './api/memoized';
 
 ReactDOM.render(
   <div>
@@ -11,18 +18,24 @@ ReactDOM.render(
       <li><a href="#/">Home</a></li>
       <li><a href="#/useState">UseState</a></li>
       <li><a href="#/useEffect">UseEffect</a></li>
+        <li><a href="#/useRef">UseRef</a></li>
       <li><a href="#/useContext">UseContext</a></li>
-      <li><a href="#/useReducer">UseReducer</a></li>
       <li><a href="#/useCallback">UseCallback</a></li>
+        <li><a href="#/useReducer">UseReducer</a></li>
       <li><a href="#/useMemo">UseMemo</a></li>
-      <li><a href="#/useRef">UseRef</a></li>
-      <li><a href="#/mobx">Mobx</a></li>
       <li><a href="#/memoized">Memoized</a></li>
     </ul>
     <HashRouter>
       <Switch>
         <Route exact path="/" component={App}></Route>
         <Route exact path="/useState" component={UseState}></Route>
+        <Route exact path="/useEffect" component={UseEffect}></Route>
+        <Route exact path="/useRef" component={UseRef}></Route>
+        <Route exact path="/useContext" component={UseContext}></Route>
+        <Route exact path="/useCallback" component={UseCallback}></Route>
+        <Route exact path="/useReducer" component={UseReducer}></Route>
+        <Route exact path="/useMemo" component={UseMemo}></Route>
+        <Route exact path="/memoized" component={Memoized}></Route>
       </Switch>
     </HashRouter>
   </div>,
