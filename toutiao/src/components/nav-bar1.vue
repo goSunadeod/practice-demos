23e78357745b79a19ed8f200cdf36e243c764171
sundae <haiqiang.xu@hand-china.com>
2020-04-06 17:28:05 +0800

    完善 navbar

--- toutiao/src/components/nav-bar1.vue
+++ toutiao/src/components/nav-bar1.vue
@@ -36,13 +36,9 @@ export default create({
       ],
     };
   },
-  created() {
-    this.prevOffset = 0; // 保存上次滚动的偏移
-  },
   methods: {
     hanlderNav(i) {
       this.active = i;
-      //   this.animate(i);
       this.scrollIntoView();
     },
     scrollIntoView(immediate) {
@@ -51,26 +47,7 @@ export default create({
       const to = title.offsetLeft - (nav.offsetWidth - title.offsetWidth) / 2;
      
       scrollLeftTo(nav, to, immediate ? 0 : +0.3);
-    },
-    animate(i) {
-      const el = this.$refs[`wrap${i}`][0];
-      const elWith = el.offsetWidth;
-      const elLeftDistance = el.getBoundingClientRect().left; // 动态获取元素距离浏览器左边的距离
-      const viewport = window.innerWidth;
-      const terminal = (viewport - elWith) / 2; // 中间到达浏览器边框的距离
-      const wrapWidth = this.$refs.wrap.offsetWidth;
-      const limit = wrapWidth - viewport; // 向左滚动的最大距离，限制为limit 向右限制为0
-      let prefix = elLeftDistance - terminal; // 需要滚动的距离
-      prefix += this.prevOffset;
-      if (prefix >= 0) {
-        prefix = Math.min(prefix, limit);
-        this.trans.transform = `translateX(${-prefix}px)`;
-      } else {
-        prefix = Math.min(-prefix, 0);
-        this.trans.transform = `translateX(${prefix}px)`;
-      }
-      this.prevOffset = Math.abs(prefix);
-    },
+    }
   },
 });
 </script>
@@ -79,6 +56,10 @@ export default create({
 @import "assets/scss/variable.scss";
 @import "assets/scss/mixins.scss";
 
+::-webkit-scrollbar {
+    width: 0;
+    background: transparent;
+}
 .tt-navbar1 {
   overflow: hidden;
   &__wrapper {
@@ -89,6 +70,10 @@ export default create({
     background-color: #fff;
     user-select: none;
     @include no-wrap;
+    overflow-x: auto;
+    &::-webkit-scrollbar {
+          display: none;
+      }
     a {
       display: block;
       -webkit-box-flex: 0;
