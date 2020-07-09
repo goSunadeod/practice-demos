39d0302c3bb439ba7168252bfc554ebcb323c269
xuhaiqiang <xuhaiqiang@xiaoheiban.cn>
2020-07-09 17:01:58 +0800

    add react 微应用

--- qiankun-demo/main/src/main.js
+++ qiankun-demo/main/src/main.js
@@ -56,6 +56,18 @@ registerMicroApps([
       }
     }
   },
+  /**
+ * name: 微应用名称 - 具有唯一性
+ * entry: 微应用入口 - 通过该地址加载微应用
+ * container: 微应用挂载节点 - 微应用加载完成后将挂载在该节点上
+ * activeRule: 微应用触发的路由规则 - 触发路由规则后将加载该微应用
+ */
+  {
+    name: "react-three",
+    entry: "//localhost:6663",
+    container: "#micro-view",
+    activeRule: "/react-three",
+  },
 ],
   {
     beforeLoad: [
